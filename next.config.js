32225279e90145e28eb4a1df0ef2a6d090286c3e
nicobeltra <nicolas.beltra@hotmail.com>
2026-05-25 13:46:57 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [{ protocol: 'https', hostname: '*.supabase.co' }],
+    remotePatterns: [
+      { protocol: 'https', hostname: '*.supabase.co' },
+    ],
+    formats: ['image/avif', 'image/webp'],
   },
 };
 module.exports = nextConfig;
